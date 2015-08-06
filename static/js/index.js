744b4b8a4eb97bea73b650addd34780f9a20cc9c
ting.wu <ting.wu@corp.netease.com>
2015-08-06 18:50:23 +0800

    svn works now.

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,9 @@ var file_tree_init = false;
 var server_ip = "";
 var server_port = "";
 var btn_state = "stop";
+var show_diff = false;
+var diff_list = [];
+var latest_cov_data = null;
 $('#btn-connect').on('click', function(){
   var $btn = $(this).button('loading');
   // check if the socket is already connected.
@@ -74,15 +77,32 @@ $('#btn-connect').on('click', function(){
       var full_file_path = path + selected_file;
       var cov = obj[full_file_path];
       if(cov){
-        var exec = cov['executed'];
-        var missing = cov['missed'];
-        var coverage = cov['coverage'];
-        $.each(exec, function(i){
-          $('.number' + exec[i]).removeClass('mis').addClass('run');
-        });
-        $.each(missing, function(i){
-          $('.number' + missing[i]).removeClass('run').addClass('mis');
-        });
+        // check if we only need to show diff result
+        if(show_diff){
+          var exec1 = cov['executed'];
+          var exec = $(exec1).filter(diff_list);
+          var missing = $(diff_list).not(exec);
+          $.each(exec, function(i){
+            $('.number' + exec[i]).removeClass('mis').addClass('run');
+          });
+          $.each(missing, function(i){
+            $('.number' + missing[i]).removeClass('run').addClass('mis');
+          });
+          $('#diff-cov').text((parseFloat(exec.length / diff_list.length) * 100).toFixed(2) + "%");
+        } else {
+          var exec = cov['executed'];
+          var missing = cov['missed'];
+          var coverage = cov['coverage'];
+          $.each(exec, function(i){
+            $('.number' + exec[i]).removeClass('mis').addClass('run');
+          });
+          $.each(missing, function(i){
+            $('.number' + missing[i]).removeClass('run').addClass('mis');
+          });
+          $('#source-cov').text((parseFloat(coverage) * 100).toFixed(2) + "%");
+        }
+      } else {
+        $('#source-cov').text(0);
       }
     }
   }
@@ -92,7 +112,7 @@ SyntaxHighlighter.defaults['toolbar'] = false;
 SyntaxHighlighter.defaults['quick-code'] = false;
 SyntaxHighlighter.all();
 
-
+// build the left sidebar 
 function build_file_tree(parent, childs){
   var total_code_lines = 0;
   var html = "";
@@ -123,6 +143,7 @@ function build_file_tree(parent, childs){
   return parent_html + html;
 }
 
+// process list directories response from jsonp
 function process_list_dir(response){
   path = response['path'];
   var dirs = response['dirs'];
@@ -148,12 +169,16 @@ function process_list_dir(response){
   });
 }
 
+// process the source text from jsonp
 function process_file_source(response){
   var filename = response['filename'];
   filename = filename.replace(/\\/g, '/');
   var source = response['text'];
+  var revision = response['Revision'];
   $('#source').attr('value', filename);
   $('#source').html('<pre class="brush:python;">' + source + '</pre>');
+  $('#source-name').text(filename);
+  $('#source-revision').text(revision);
   SyntaxHighlighter.highlight();
 }
 
@@ -188,6 +213,7 @@ function show_coverage(data){
   }
 }
 
+// funny
 $("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
@@ -258,5 +284,31 @@ $('#stop_trace').on('click', function(){
   sendMessage(JSON.stringify({
     "op": "stop_trace"
   }));
+});
+
+$('#chk-show-diff').on('click', function(){
+  show_diff = $(this).is(':checked');
+  if(show_diff){
+    // remove the original coverage info
+    $('#source .line').removeClass('mis').removeClass('run');
+    var filename = $('#source').attr('value');
+    if(!filename || filename == "") return;
+    filename = encodeURIComponent(filename);
+    var cur_version = $("#source-revision").text();
+    var old_version = $("#txt-diff-version").val();
+    if(old_version > cur_version){
+      alert("别闹了~");
+      return;
+    }
+    // get diff info
+    $.ajax({
+      url: "http://" + server_ip + ":5000/diff?path=" + filename + '&old=' + old_version + '&cur=' + cur_version,
+      jsonp: 'callback',
+      dataType: "jsonp",
+      success: function(data){
+        diff_list = data['update'];
+      }
+    });
+  }
 });
 
